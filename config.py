3386257c373414d82c846fe80de45c7429dd8d11
Your Name <you@example.com>
2023-03-01 11:50:38 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cvyrc56fzszp.ap-southeast-1.rds.amazonaws.com"
+customuser = "rajendra"
+custompass = "rajendra123"
+customdb = "employee"
+custombucket = "aws-project-employee"
+customregion = "ap-southeast-1"
